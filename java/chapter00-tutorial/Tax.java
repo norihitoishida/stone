c78df1d6f199765d885c9d04c51ce11dbea55033
norihitoishida <norihitoishida@gmail.com>
2020-12-13 23:11:05 +0900

    update

---
+++ java/chapter00-tutorial/Tax.java
@@ -0,0 +1,8 @@
+public class Tax {
+    public static void main(String[] args) {
+        int x = 1000;
+        double tax = 1.08;
+        double price = x * tax;
+        System.out.println(price);
+    }
+}
